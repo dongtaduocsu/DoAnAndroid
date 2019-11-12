38b507d0a1a9acfcdc8fb8641266ca9e69753326
pns1681999 <43614413+pns1681999@users.noreply.github.com>
2019-11-12 22:39:07 +0700

    Hoàn thành video, chưa them tính năng mới

--- app/src/main/java/com/example/doanandroid/Fragment/fragment_timkiem.java
+++ app/src/main/java/com/example/doanandroid/Fragment/fragment_timkiem.java
@@ -1,22 +1,104 @@
 package com.example.doanandroid.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.doanandroid.Adapter.SearchBaiHatAdapter;
+import com.example.doanandroid.Model.Baihat;
 import com.example.doanandroid.R;
+import com.example.doanandroid.Service.APIservice;
+import com.example.doanandroid.Service.Dataservice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class fragment_timkiem extends Fragment {
     View view;
+    Toolbar toolbarsearchbaihat;
+    RecyclerView recyclerViewsearchbaihat;
+    TextView txtkhongcodulieu;
+    SearchBaiHatAdapter searchBaiHatAdapter;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view=inflater.inflate(R.layout.timkiem_fragment,container,false);
+        toolbarsearchbaihat = view.findViewById(R.id.toolbarsearchbaihat);
+        recyclerViewsearchbaihat = view.findViewById(R.id.recyclerviewsearchbaihat);
+        txtkhongcodulieu = view.findViewById(R.id.textviewkhongcodulieu);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbarsearchbaihat);
+        toolbarsearchbaihat.setTitle("");
+        setHasOptionsMenu(true);
         return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.search_view, menu);
+        MenuItem menuItem = menu.findItem(R.id.menu_search);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+        searchView.setMaxWidth(Integer.MAX_VALUE);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                SearchTuKhoaBaiHat(s);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    private void SearchTuKhoaBaiHat (String query){
+        Dataservice dataservice = APIservice.getservice();
+        Call<List<Baihat>> callback = dataservice.GetSearchBaihat(query);
+        callback.enqueue(new Callback<List<Baihat>>() {
+            @Override
+            public void onResponse(Call<List<Baihat>> call, Response<List<Baihat>> response) {
+                ArrayList<Baihat> mangbaihat = (ArrayList<Baihat>) response.body();
+                if (mangbaihat.size()>0){
+                    searchBaiHatAdapter = new SearchBaiHatAdapter(getActivity(), mangbaihat);
+                    LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
+                    recyclerViewsearchbaihat.setLayoutManager(linearLayoutManager);
+                    recyclerViewsearchbaihat.setAdapter(searchBaiHatAdapter);
+                    txtkhongcodulieu.setVisibility(View.GONE);
+                    recyclerViewsearchbaihat.setVisibility(View.VISIBLE);
+                }else {
+                    recyclerViewsearchbaihat.setVisibility(View.GONE);
+                    txtkhongcodulieu.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Baihat>> call, Throwable t) {
+
+            }
+        });
     }
 }
